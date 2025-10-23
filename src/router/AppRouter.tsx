9ff52ba8c1e2d7db8c921d8fc050ac49609373a1
Ricardo de Jesus Zapata Cruz <ricardozapatacruz@gmail.com>
2025-10-23 15:41:39 -0500

    Ricardo (#18)
    * Not found view
    
    * Complete welcome view
    
    * Update login and register
    
    * Fix build bug

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,36 +1,60 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import PublicRoute from "../components/router/PublicRoute";
 import ProtectedRoute from "../components/router/ProtectedRoute";
 import AuthLayout from "../layout/AuthLayout";
 import MainLayout from "../layout/MainLayout";
-import LoginPage from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
 import WelcomePage from "../pages/auth/Welcome";
 import SiteMapPage from "../pages/SiteMap";
+import NotFound from "../pages/NotFound";
+import LoginModal from "../components/modals/LoginModal";
+import RegisterModal from "../components/modals/RegisterModal";
 import { Toaster } from "sonner";
+
+function AppRoutes() {
+  const location = useLocation();
+  const background = location.state?.background;
+
+  return (
+    <>
+      {/* Rutas principales */}
+      <Routes location={background || location}>
+        {/* Public routes */}
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="auth" element={<AuthLayout />}>
+          </Route>
+          <Route path="mapa-sitio" element={<SiteMapPage />} />
+        </Route>
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="dashboard" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Route>
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {/* Modales flotantes - solo se renderizan si hay background */}
+      {background && (
+        <Routes>
+          <Route path="/iniciar-sesion" element={<LoginModal />} />
+          <Route path="/registrarse" element={<RegisterModal />} />
+        </Routes>
+      )}
+    </>
+  );
+}
 
 export default function AppRouter() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route path="/bienvenida" element={<WelcomePage />} />
-          <Route path="/mapa-sitio" element={<SiteMapPage />} />
-          
-          <Route element={<PublicRoute />}>
-            <Route path="/login" element={<AuthLayout />}>
-              <Route index element={<LoginPage />} />
-            </Route>
-          </Route>
-          
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<Dashboard />} />
-            </Route>
-          </Route>
-        </Routes>
+        <AppRoutes />
         <Toaster position="bottom-right" richColors />
       </BrowserRouter>
     </AuthProvider>
